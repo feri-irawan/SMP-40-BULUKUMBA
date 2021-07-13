d043e3ad68eb394101e6191b332fc4019cf7a2fc
feri-irawan <feriirawans1998@gmail.com>
2021-07-13 10:18:48 +0800

    mengubah isi file

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+
 const app = express()
 const port = 3000
+
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
 
 app.get('/', (req, res) => {
   res.render('app/index')
