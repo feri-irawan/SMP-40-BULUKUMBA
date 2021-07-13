6310642deb72abd7942c006fe26b67a8ab82cb80
feri-irawan <feriirawans1998@gmail.com>
2021-07-13 12:50:15 +0800

    Mengubah isi file index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
 const port = 3000
 
 
 app.get('/', (req, res) => {
-  res.sendFile('views/index.html', {
-    root: __dirname
-  })
+  res.sendFile(path.join(__dirname+'/views/index.html'))
+})
+
+app.use('/', (req, res) => {
+  res.send('<h1>404 | Not Found</h1>')
 })
 
 app.listen(port, () => {
